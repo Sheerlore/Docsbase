089faeb59491fb9bc865c0c5f19654cf46abe1dd
uai <noema01@outlook.jp>
2020-10-21 12:40:45 +0900

    first commit

---
+++ js/setting.js
@@ -0,0 +1,16 @@
+const APPNAME = "アプリ名";
+const GITHUBURL = "#";
+
+
+
+export class App {
+    appName = APPNAME;
+    githubUrl = GITHUBURL;
+    constructor(appName, githubUrl) {
+        this.appName = this.appName
+    }
+}
+
+
+
+
