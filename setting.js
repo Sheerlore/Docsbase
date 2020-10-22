5256256402a271823b43900d9cd29db76f4399a3
uai <noema01@outlook.jp>
2020-10-22 13:49:23 +0900

    modify small

--- setting.js
+++ setting.js
@@ -1,15 +1,22 @@
+// --------------------------------------------
+// APPNAME: アプリ名
+// GITHUB_REPO_URL: githubのrepositoryのURL
+// CONTENT_TABEL: サイドバーに表示するものの設定。配列の中にmapで指定
+//      "title" : サイドバーの表示名
+//      "url" : contentディレクトリの中からのパス
+// --------------------------------------------
 export const APPNAME = "APP NAME";
 export const GITHUB_REPO_URL = "https://github.com/";
 export let CONTENT_TABLE = [{
-        "title": "title1",
-        "url": "#"
+        "title": "content1",
+        "url": "content/content1.html"
     },
     {
-        "title": "title2",
-        "url": "#"
+        "title": "content2",
+        "url": "content/content2.html"
     },
     {
-        "title": "title3",
-        "url": "#"
+        "title": "content3",
+        "url": "content/content3.html"
     }
 ]
